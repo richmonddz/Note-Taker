a7a4f4a9d69eaea644a41500217a69ddf8bec954
Richmond Dominguez <richmonddominguez@gmail.com>
2022-06-08 15:01:23 -0700

    added  route to grab notes.html

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -5,3 +5,11 @@ var fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
