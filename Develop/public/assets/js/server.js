b6f0eebae98d8746c38eec8142bac08fe56b44d6
Richmond Dominguez <richmonddominguez@gmail.com>
2022-06-08 17:07:57 -0700

    added route to index.html

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -13,3 +13,11 @@ app.use(express.json());
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/assets/index.html"));
+});
