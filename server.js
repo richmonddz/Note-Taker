8adb2584c639d6a9ada353c42382a24284ae7ad7
Richmond Dominguez <richmonddominguez@gmail.com>
2022-06-08 21:55:52 -0700

    finalized server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -21,3 +21,24 @@ app.get("/api/notes", (req, res) => {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let notelength = noteList.length.toString();
+  newNote.id = notelength;
+  noteList.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  res.json(noteList);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteId = req.params.id.toString();
+  noteList = noteList.filter((selected) => {
+    return selected.id != noteId;
+  });
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  res.json(noteList);
+});
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
